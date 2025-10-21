a360e8bedc324e8fd1af9875444b9bb4f50846c9
Henrique-Linhares <hlinhares453@gmail.com>
2025-10-21 16:17:59 -0300

    Iniciando o Exercício 01

---
+++ listaDeExercicios/src/exercicio01/Funcionario.java
@@ -0,0 +1,15 @@
+//Exercício 1: Crie uma classe abstrata chamada Funcionario que possua os
+//seguintes atributos: nome, salario. Defina um método abstrato
+//calcularBonus(). Crie classes concretas Gerente e Desenvolvedor, que
+//estendem Funcionario, e implementam o cálculo do bônus de forma
+//diferente. Instancie objetos dessas classes e calcule o bônus para cada tipo
+//de funcionário.
+
+package exercicio01;
+
+public abstract class Funcionario {
+    private String nome;
+    private String salario;
+
+
+}
